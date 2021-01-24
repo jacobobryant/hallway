b873697b440dc6c38f8196fd62b1a10d170b17fd
Jacob O'Bryant <foo@jacobobryant.com>
2021-01-24 04:59:19 -0800

    Add meta thing for mobile

--- src/hallway/static.clj
+++ src/hallway/static.clj
@@ -12,6 +12,7 @@
   [:html
    [:head
     [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     [:title "Hallway"]
     [:link {:rel "stylesheet" :href "/css/main.css"}]]
    [:body contents simple-analytics]])
