65035559faf444a08f858b86380e31ec2b26851c
Jacob O'Bryant <git@jacobobryant.com>
2021-07-26 13:50:02 -0700

    Remove session store

--- src/findka/hallway.clj
+++ src/findka/hallway.clj
@@ -3,22 +3,57 @@
             [biff.misc :as misc]
             [biff.util :as bu]
             [findka.hallway.env :refer [use-env]]
-            [findka.hallway.routes :refer [routes on-error]])
+            [findka.hallway.routes :refer [routes on-error]]
+            [muuntaja.middleware :as muuntaja]
+            [ring.middleware.defaults :as rd]
+            [ring.middleware.session.memory :as mem])
   (:gen-class))
+
+;; Modified from biff.middleware/wrap-defaults since we don't need sessions.
+(defn wrap-defaults [handler {:keys [secure on-error env]
+                              :or {secure true}
+                              :as opts}]
+  (let [changes {[:session]                false
+                 [:security :anti-forgery] false
+                 [:security :ssl-redirect] false
+                 [:static]                 false}
+        ring-defaults (reduce (fn [m [path value]]
+                                (assoc-in m path value))
+                              (if secure
+                                rd/secure-site-defaults
+                                rd/site-defaults)
+                              changes)]
+    (-> handler
+        (mid/wrap-env env)
+        mid/wrap-flat-keys
+        muuntaja/wrap-params
+        muuntaja/wrap-format
+        (mid/wrap-resource {})
+        (rd/wrap-defaults ring-defaults)
+        (mid/wrap-internal-error {:on-error on-error})
+        mid/wrap-log-requests)))
+
+(defn use-default-middleware [{:keys [biff.middleware/secure biff/on-error]
+                               :or {secure true}
+                               :as sys}]
+  (update sys :biff/handler wrap-defaults
+          {:secure secure
+           :env sys
+           :on-error on-error}))
 
 (def components
   [use-env
    misc/use-nrepl
    misc/use-reitit
-   mid/use-default-middleware
+   use-default-middleware
    misc/use-jetty
    (fn [{:keys [biff/base-url] :as sys}]
      (println "Go to" base-url)
      sys)])
 
-(def config {:biff.reitit/routes       (fn [] (routes))
-             :biff/after-refresh       `-main
-             :biff/on-error            (fn [req] (on-error req))})
+(def config {:biff.reitit/routes (fn [] (routes))
+             :biff/after-refresh `-main
+             :biff/on-error      (fn [req] (on-error req))})
 
 (defn -main []
   (bu/start-system config components))
--- src/findka/hallway/routes.clj
+++ src/findka/hallway/routes.clj
@@ -1,5 +1,11 @@
 (ns findka.hallway.routes
-  (:require [findka.hallway.views :as v]))
+  (:require [biff.util :as bu]
+            [findka.hallway.views :as v]))
+
+(defn on-error [{:keys [status]}]
+  {:status status
+   :headers/Content-Type "text/html"
+   :body (str "<h1>" (get bu/http-status->msg status "There was an error.") "</h1>")})
 
 (defn routes []
   [
